7df8dd4272a70e10df7d6addf6aca7a431aafa83
Mayur Santosh Dhole <mayursdwin11@gmail.com>
2026-01-01 21:09:58 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,14 +1,4 @@
-FINAL_MESSAGE = """
-You reached the end, not because you are perfect,
-but because you kept going even when it was hard.
-
-This world is better with you in it.
-Never forget that someone believes in you deeply.
-"""
-
-ENCOURAGEMENTS = [
-    "Take a breath. You’re doing better than you think.",
-    "Even small steps count.",
-    "You don’t have to be strong all the time.",
-    "Rest if you need to. Don’t quit."
-]
+streamlit==1.29.0
+fastapi==0.104.1
+uvicorn==0.24.0.post1
+requests==2.31.0
