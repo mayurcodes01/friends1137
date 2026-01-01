379f11b692c17b126fdddc33f6bc530b106c5dea
Mayur Santosh Dhole <mayursdwin11@gmail.com>
2026-01-01 20:51:21 +0530

    Add requirements.py file

---
+++ requirements.py
@@ -0,0 +1,14 @@
+FINAL_MESSAGE = """
+You reached the end, not because you are perfect,
+but because you kept going even when it was hard.
+
+This world is better with you in it.
+Never forget that someone believes in you deeply.
+"""
+
+ENCOURAGEMENTS = [
+    "Take a breath. You’re doing better than you think.",
+    "Even small steps count.",
+    "You don’t have to be strong all the time.",
+    "Rest if you need to. Don’t quit."
+]
